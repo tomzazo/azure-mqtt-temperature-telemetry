3de5b59d175dea011317e14927251f4d622a40e8
Tomaz Zazijal <tomaz.zazijal@gmail.com>
2023-07-19 23:24:30 +0200

    Fixed:
    - interval of reading temperature from the sensor

--- scripts/get-temp.sh
+++ scripts/get-temp.sh
@@ -1,8 +1,4 @@
 #!bin/sh
 
-while true
-do
-    cat /sys/devices/w1_bus_master1/28-*/w1_slave > /home/pi/deploy/sensors/temperature/reading
+cat /sys/devices/w1_bus_master1/28-*/w1_slave > /home/pi/deploy/sensors/temperature/reading
 
-    sleep 60
-done
